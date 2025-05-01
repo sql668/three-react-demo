5ec4f169829e11b1270834e5f9180eab5a4e178d
sql123 <mike@magic.org>
2025-05-01 16:51:21 +0800

    依赖升级

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import requireTransform from "vite-plugin-require-transform";
 
-//console.log("路径地址1:",path.resolve(__dirname, '../packages/antd/src/index.tsx'));
-//console.log("路径地址2:",path.resolve(__dirname, './src/components/*'));
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
